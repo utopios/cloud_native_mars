8044653e3aff5358fb0139337135e4f54dfdac2d
ihab abadi <ihab@utopios.net>
2024-03-20 12:12:15 +0100

    avec le script de démarrage

---
+++ quarkus-super-heroes/run-app-with-jeager.sh
@@ -0,0 +1,5 @@
+## Docker compose pour démarrer les conteneurs
+
+docker compose -f deploy/docker-compose/native.yml up -d
+
+docker compose -f deploy/docker-compose/monitoring.yml up -d
---
+++ quarkus-super-heroes/stop-app.sh
@@ -0,0 +1,6 @@
+## Arreter les conteneurs avec docker compose
+
+
+docker compose -f deploy/docker-compose/native.yml up down
+
+docker compose -f deploy/docker-compose/monitoring.yml down
